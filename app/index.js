400405987fe289b9ba90e430886b902aea9d886e
Aleksandr Kornienko <aleks@fugue.co>
2017-07-04 01:57:56 -0700

    Changed Hello World message2

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!2'));
 
 if (!module.parent) {
     // eslint-disable-next-line no-console
